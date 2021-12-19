052d9c0b135b831fe0a347182fcb3d6498499a24
LZx <l.mikespace@outlook.com>
2021-12-19 12:38:54 +0800

    只消费JSON数据

--- gateway-infrastructure/src/main/java/org/lzx/web/gateway/infrastructure/config/spring/security/userdetails/manager/UserDetailsClient.java
+++ gateway-infrastructure/src/main/java/org/lzx/web/gateway/infrastructure/config/spring/security/userdetails/manager/UserDetailsClient.java
@@ -1,6 +1,7 @@
 package org.lzx.web.gateway.infrastructure.config.spring.security.userdetails.manager;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "userDetailsClient", url = "http://localhost:9000", configuration = ClientConfig.class)
 public interface UserDetailsClient {
 
-    @GetMapping(value = "/users/{username}")
+    @GetMapping(value = "/users/{username}", consumes = MediaType.APPLICATION_JSON_VALUE)
     String findByUsername(@PathVariable("username") String username);
 
 }
